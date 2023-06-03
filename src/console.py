649cf4433fa540eafbf1bb28f45933e8301f6052
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-06-03 11:40:46 -0400

    update reading to include lambda_handler migration

--- src/console.py
+++ src/console.py
@@ -3,14 +3,16 @@ from query_bucket.athena_boto import *
 
 def lambda_handler(event, context):
     time = datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")
-    
+    print('called at', time)
+
     output_bucket_name = "jigsawtexasresults" # change to your output bucket
     db_name = "txreceipts" # change to your db
     output_folder = "output" 
+
+    # change jigsawtexasquery to your input bucket
     query = "SELECT * FROM jigsawtexasquery limit 3"
     output_bucket_folder = f"s3://{output_bucket_name}/{output_folder}/"
     
-    print('called at', time)
     query_response = query_athena(query,
                                db_name, output_bucket_folder)
     
@@ -18,5 +20,5 @@ def lambda_handler(event, context):
     print(results_df)
     return results_df.to_dict('records')
 
-# change jigsawtexasquery to your input bucket
-# query_results("SELECT * FROM jigsawtexasquery limit 3")
+
+# lambda_handler({}, {})
